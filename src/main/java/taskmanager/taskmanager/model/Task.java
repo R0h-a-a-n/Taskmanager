32ae03679b928b5cc82168e3c1add6cbfee25334
Rohaan <yourworstfear178@gmail.com>
2025-10-10 00:25:59 +0530

    first commit

---
+++ src/main/java/taskmanager/taskmanager/model/Task.java
@@ -0,0 +1,22 @@
+package taskmanager.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    private String description;
+
+    private boolean completed = false;
+}
